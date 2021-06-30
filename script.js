aedd98d9401812cc0855f0d4d1b95f9358062f20
taufiktr04 <taufiktr04@gmail.com>
2021-06-30 16:17:31 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+const button = document.querySelector(".tombol");
+button.addEventListener("click", function () {
+  window.location.href = "https://www.youtube.com/channel/UC9-U6RPBIG2q4xQPk-W8miw";
+});
